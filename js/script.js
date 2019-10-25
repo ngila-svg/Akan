6ac0b2557cba616683553c4cfd523250a8a405ab
ngila-svg <musyokawilfred6@gmail.com>
2019-10-25 11:16:48 +0300

    add styles to css

--- js/script.js
+++ js/script.js
@@ -20,4 +20,26 @@ function outputNames(){
         alert("Enter a valid date");
     }
 
+    // This will calculate the specific day of the month
+    var century=(year-1)/100+1;
+    var dayOfWeek=Math.floor( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
+    var theMaleGender=document.getElementById("male");
+    var theFemaleGender=document.getElementById("female");
+  
+    // check  for gender inout
+    if(theMaleGender.checked){
+        var theMaleValue = theMaleGender.value;
+    }else if(theFemaleGender.checked){
+        var theFemaleValue = theFemaleGender.value;
+    }
+    // check if the value returned is correct and then display the matching akan
+    if(theMaleValue==="male"){
+
+        document.getElementById("display").innerHTML="Your Akan Name is "+maleNames[dayOfWeek];
+
+    }else{
+
+        document.getElementById("display").innerHTML="Your Akan Name is "+femaleNames[dayOfWeek];
+    }
     
+}
