ab87f2fe68fcc96e669a93f8a547654e48749a86
ngila-svg <musyokawilfred6@gmail.com>
2019-10-25 11:10:40 +0300

    add intouts for variable declaration

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@
 var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 
+// create a function that would be called in the HTML file
+function outputNames(){
+
+    // Declaring variable intouts
+    var date=parseFloat(document.getElementById("Date").value);
+    var month=parseFloat(document.getElementById("Month").value);
+    var year=parseFloat(document.getElementById("Year").value);
+    
